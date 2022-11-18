e85d757828d7579784962d464b4cecfbfee02d76
jpope229 <jpope229@mtroyal.ca>
2022-11-18 04:01:26 -0700

    sample

--- starting-files/song.js
+++ starting-files/song.js
@@ -1,6 +1,6 @@
 const genres = JSON.parse(genreList);
 const artists = JSON.parse(artistList);
-
+const songs = JSON.parse(songList);
 document.addEventListener("DOMContentLoaded", function() {
 
     for (let a of artists) {
@@ -8,18 +8,54 @@ document.addEventListener("DOMContentLoaded", function() {
         option.text = a["name"];
         option.value = a["id"];
         let aSelect = document.getElementById('artist');
-        console.log(option);
         aSelect.appendChild(option);
         }
 
-        for (let g of genres) {
-            let option = document.createElement("option");
-            option.text = g["name"];
-            option.value = g["id"];
-            let aSelect = document.getElementById('genre');
-            console.log(option);
-            aSelect.appendChild(option);
-            }
+    for (let g of genres) {
+        let option = document.createElement("option");
+        option.text = g["name"];
+        option.value = g["id"];
+        let aSelect = document.getElementById('genre');
+        aSelect.appendChild(option);
+        }
+    
+    //Retrieve and display song data
+    const thead = document.getElementById('headerRow');
+    
+    for (let s of songs){
+        const songRow = document.createElement("tr");
+        songRow.setAttribute("songID", s["song_id"]);
+        
+        //create td for title 
+        const songDataTitle = document.createElement("td");
+        songDataTitle.innerText(s["title"]);
+        console.log(s["title"]);
+        songRow.appendChild(songDataTitle);
 
+        //create td for artist name 
+        const songDataArtist = document.createElement("td");
+        songDataTitle.textContent = s.artist.name;
+        songRow.appendChild(songDataArtist);
 
-});
+        //create td for year 
+        const songDataYear = document.createElement("td");
+        songDataTitle.textContent = s.year;
+        songRow.appendChild(songDataYear);
+
+        //create td for genre
+        const songDataGenre = document.createElement("td");
+        songDataTitle.textContent = s.genre.name;
+        songRow.appendChild(songDataGenre);
+
+        //create td for popularity
+        const songDataPopularity = document.createElement("td");
+        songDataTitle.textContent = s.details.popularity;
+        songRow.appendChild(songDataPopularity);
+
+        console.log(songRow);
+        thead.insertAdjacentElement('afterend', songRow);
+
+        }
+    }
+
+);
